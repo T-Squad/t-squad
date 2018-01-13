efd544bb95c1391bf7203b064649d0210404c65e
Jason M. Goncalves <jgoncalves@protonmail.com>
2018-01-13 12:46:34 -0500

    removed wierd characters on both the index.html page and app.js page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,3 @@
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
 // set variables for API Keys
 
 // YELP API INFORMATION
@@ -22,7 +18,7 @@ function init() {
     // we want to get the location from our user
     let appLocation;
     $.getJSON('https://ipinfo.io', function(data) {
-<<<<<<< Updated upstream
+
         console.log(data);
         appLocation = data.loc.split(',');
         console.log(appLocation[0]);
@@ -52,66 +48,65 @@ function init() {
             complete: function(response) {
                 console.log(response)
             }
-        // }).done(response, => {
-        //  console.log(response);
-        }).done(function(data){
+            // }).done(response, => {
+            //  console.log(response);
+        }).done(function(data) {
             let businessText = JSON.parse(data);
             $('#locationInfo').text(businessText);
         });
     });
-=======
-            console.log(data);
-            appLocation = data.loc.split(',');
-            console.log(appLocation[0]);
-            console.log(appLocation[1]);
-            let lat = appLocation[0];
-            let long = appLocation[1];
-            $('#locationInfo').text(data.city);
 
-            // yelp api key
-            const YELP_API = window.yelpKey
+    console.log(data);
+    appLocation = data.loc.split(',');
+    console.log(appLocation[0]);
+    console.log(appLocation[1]);
+    let lat = appLocation[0];
+    let long = appLocation[1];
+    $('#locationInfo').text(data.city);
 
-            console.log(YELP_API);
+    // yelp api key
+    const YELP_API = window.yelpKey
 
-            // proxy URL to fix CORS issue
-            const proxyURL = 'https://shielded-hamlet-43668.herokuapp.com/';
+    console.log(YELP_API);
 
-            const yelpURL = 'https://api.yelp.com/v3/businesses/search?term=pizza&latitude=' + lat + '&longitude=' + long + '&limit=10';
-            $.ajax({
-                url: proxyURL + yelpURL,
-                method: 'GET',
-                // headers: {
-                //  authorization: 'Bearer' + YELP_API
-                // }
-                beforeSend: function(xhr, settings) {
-                    xhr.setRequestHeader('Authorization', 'Bearer ' + YELP_API);
-                },
-                complete: function(response) {
-                    console.log(response)
-                }
-                // }).done(response, => {
-                //  console.log(response);
-            }).done(function(data) {
-                console.log(data);               
-                // let img = $('<img>').attr('src', data.businesses[0]['image_url']);
-                // let businessText = JSON.parse(data.);
+    // proxy URL to fix CORS issue
+    const proxyURL = 'https://shielded-hamlet-43668.herokuapp.com/';
 
-                let yelpInfo = $('#secondaryContents');
-                let cardInfo = $('.card-title');
+    const yelpURL = 'https://api.yelp.com/v3/businesses/search?term=pizza&latitude=' + lat + '&longitude=' + long + '&limit=10';
+    $.ajax({
+        url: proxyURL + yelpURL,
+        method: 'GET',
+        // headers: {
+        //  authorization: 'Bearer' + YELP_API
+        // }
+        beforeSend: function(xhr, settings) {
+            xhr.setRequestHeader('Authorization', 'Bearer ' + YELP_API);
+        },
+        complete: function(response) {
+            console.log(response)
+        }
+        // }).done(response, => {
+        //  console.log(response);
+    }).done(function(data) {
+        console.log(data);
+        // let img = $('<img>').attr('src', data.businesses[0]['image_url']);
+        // let businessText = JSON.parse(data.);
 
-                $('.card-title').append(data.businesses[0].name);
-                $('.card-img-top').attr('src', data.businesses[0]['image_url']);
-                $('.card-title').append(data.businesses[1].name);
-                $('.card-img-top').attr('src', data.businesses[1]['image_url']);
-                $('.card-title').append(data.businesses[2].name);
-                $('.card-img-top').attr('src', data.businesses[2]['image_url']);
-                $('.card-title').append(data.businesses[3].name);
-                $('.card-img-top').attr('src', data.businesses[3]['image_url']);
-                $('.card-title').append(data.businesses[4].name);
-                $('.card-img-top').attr('src', data.businesses[4]['image_url']);
-            });
-        });
-    }
+        let yelpInfo = $('#secondaryContents');
+        let cardInfo = $('.card-title');
 
-    init();
->>>>>>> Stashed changes
+        $('.card-title').append(data.businesses[0].name);
+        $('.card-img-top').attr('src', data.businesses[0]['image_url']);
+        $('.card-title').append(data.businesses[1].name);
+        $('.card-img-top').attr('src', data.businesses[1]['image_url']);
+        $('.card-title').append(data.businesses[2].name);
+        $('.card-img-top').attr('src', data.businesses[2]['image_url']);
+        $('.card-title').append(data.businesses[3].name);
+        $('.card-img-top').attr('src', data.businesses[3]['image_url']);
+        $('.card-title').append(data.businesses[4].name);
+        $('.card-img-top').attr('src', data.businesses[4]['image_url']);
+    });
+});
+}
+
+init();
